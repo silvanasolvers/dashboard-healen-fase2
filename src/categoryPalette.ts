321f326038a732d0e4b7162a5957fbab3a0617e9
root <root@srv1795353.hstgr.cloud>
2026-09-02 22:14:05 +0000

    Revert "Improve category color contrast"
    This reverts commit b11404b8accc06e08820d7ad8715f9d2a146bd56.

--- src/categoryPalette.ts
+++
@@ -1,6 +0,0 @@
-const CATEGORY_VISUAL_TONE_COUNT = 6;
-
-export function categoryVisualClass(index: number): string {
-  const tone = ((index % CATEGORY_VISUAL_TONE_COUNT) + CATEGORY_VISUAL_TONE_COUNT) % CATEGORY_VISUAL_TONE_COUNT;
-  return `category-visual--${tone}`;
-}
